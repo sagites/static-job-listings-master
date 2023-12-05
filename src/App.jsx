4673b72389dd32fe4f14d70c8486235302741956
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-12-05 11:55:44 +0100

    Changed a bit innit?

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 // import React from "react";
 import Slide from "./Components/slide";
 import "./App.css"
-import picture from "../public/images/bg-header-desktop.svg"
+// import picture from "../public/images/bg-header-desktop.svg"
+// import ButtonSector from "./Components/buttons";
 
 const App = ({data}) => {
   return ( 
-    <>
+    <div className="body">
       <div className="backpicture">
-        <img src={picture} alt="" />
+        <img src="../public/images/bg-header-desktop.svg" alt="" />
       </div>
       
       <div>
        {data && data.map( (item, i) => (
-              <Slide key={i} {...item}/>
+          <Slide key={i} {...item}/>
        ))}
+
+  
       </div>
-    </>
+    </div>
     
    );
 }
