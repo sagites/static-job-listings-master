7cc33645b9bcee9d9a5cc9e90d80c1d0d62c462d
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-12-05 16:31:20 +0100

    Nothing really changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
- import data from "./assets/data.json"
+import data from "./assets/data.json"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
