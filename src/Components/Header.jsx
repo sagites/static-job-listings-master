2a0b2c9facae5c083ac7964042fe1cdb37c8d5f7
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-12-08 21:58:34 +0100

    Minor is changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="">
             <picture>
-                <source srcSet="./images/bg-header-desktop.svg" media="(min-width: 1024px)" />
+                <source srcSet="./images/bg-header-desktop.svg" media="(min-width: 650px)" />
                 <img src="./images/bg-header-mobile.svg" alt="bg-header-mobile" />
             </picture>
 
