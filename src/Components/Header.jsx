c4f22c9a26043d69bf074c2e0a1d73e713fedb92
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-12-08 22:34:25 +0100

    Added the dreaded thing to students

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <div className="">
+        <div className="header">
             <picture>
                 <source srcSet="./images/bg-header-desktop.svg" media="(min-width: 650px)" />
                 <img src="./images/bg-header-mobile.svg" alt="bg-header-mobile" />
